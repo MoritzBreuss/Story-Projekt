ba021250f2b59a4a4f9bf27886658eb1a53ac95c
Lv4e <klu230103@spengergasse.at>
2024-06-23 17:44:04 +0200

    Speicherstände sind jetzt fähig geladen zu werden, Struktur von code leslicher gemacht

--- 1Page/script.js
+++ 1Page/script.js
@@ -29,7 +29,6 @@ let textStelle = 0;
 let bildStelle = 0;
 let optionenStelle = 0;
 
-
 function Spielzug() {
   if (textArray[textStelle] === "img") {
     nextBild();
@@ -44,12 +43,15 @@ function Spielzug() {
     console.log("ende von text");
   }
 
-  
-if (!localStorage.getItem("SpeicherstandArray")) { //Wenn es noch keine Speicherstände gibt, wird es erstellt
+  if (!localStorage.getItem("SpeicherstandArray")) {
+    //Wenn es noch keine Speicherstände gibt, wird es erstellt
     localStorage.setItem("SpeicherstandArray", JSON.stringify([]));
-}
-if (!localStorage.getItem("SpeicherstandAnzeige")) {            //Wenn es noch keine Speicherstandanzeige für das Menü gibt, wird es erstellt
-    localStorage.setItem("SpeicherstandAnzeige", `<h3>Deine Speicherstände</h3>
+  }
+  if (!localStorage.getItem("SpeicherstandAnzeige")) {
+    //Wenn es noch keine Speicherstandanzeige für das Menü gibt, wird es erstellt
+    localStorage.setItem(
+      "SpeicherstandAnzeige",
+      `<h3>Deine Speicherstände</h3>
           <div class="speicherstaende">
             <div onclick="slotButtonClicked(event)" id="slot1" class="slot">
                 <div class="slot-number">Leerer Slot</div>
@@ -89,8 +91,9 @@ if (!localStorage.getItem("SpeicherstandAnzeige")) {            //Wenn es noch k
               </div>
             
 
-          </div>`);
-}
+          </div>`
+    );
+  }
 }
 
 function nextBild() {
@@ -100,6 +103,12 @@ function nextBild() {
   textStelle++;
   console.log(bildStelle + "Bild changing success");
   Spielzug();
+}
+
+function showCurrentBild() {
+  //nur für den Bildwechsel bei Speicherständen verwenden!!!! sonst nextbild verwenden
+  document.body.style.backgroundImage =
+    "url('picture1/img" + bildStelle + ".jpg')";
 }
 
 function optionenErstellen() {
@@ -168,21 +177,8 @@ function openMenu() {
   menudiv.style.zIndex = 3;
   insidemenu.style.display = "block";
   insidemenu.style.zIndex = 4;
-
-    getSpeicherstandAnzeige();
+  getSpeicherstandAnzeige();
 }
-
-function getSpeicherstandAnzeige() {    //Holt die Speicherstände aus dem Localstorage und zeigt sie an
-    let currentAnzeige = document.getElementById("speicherstaende-container-id");
-    currentAnzeige.innerHTML = localStorage.getItem("SpeicherstandAnzeige");
-}
-
-function setSpeicherstandAnzeige() { //Settet die Speicherstände in den Localstorage
-    let currentAnzeige = document.getElementById("speicherstaende-container-id");
-    localStorage.setItem("SpeicherstandAnzeige", currentAnzeige.innerHTML);
-
-}
-
 
 function closeMenu() {
   //closed das menü
@@ -197,28 +193,20 @@ function closeMenu() {
   insidemenu.style.zIndex = -5;
 }
 
-function willSpeicherstandLadenFunc() {
-  willSpeicherstandLaden = !willSpeicherstandLaden;
-  speicherStandLadenAnimation();
-    
+function getSpeicherstandAnzeige() {
+  //Holt die Speicherstände aus dem Localstorage und zeigt sie an
+  let currentAnzeige = document.getElementById("speicherstaende-container-id");
+  currentAnzeige.innerHTML = localStorage.getItem("SpeicherstandAnzeige");
 }
 
-function speicherStandLadenAnimation () {
-  const buttons = document.querySelectorAll('.slot');
-  if (willSpeicherstandLaden) {
-    buttons.forEach(button => {
-        console.log('Adding bounce to:', button);
-        button.classList.add('bounce');
-    });
-} else if (!willSpeicherstandLaden || button.classList.contains('bounce')){
-    buttons.forEach(button => {
-        console.log('Removing bounce from:', button);
-        button.classList.remove('bounce');
-    });
-}
+function setSpeicherstandAnzeige() {
+  //Settet die Speicherstände in den Localstorage
+  let currentAnzeige = document.getElementById("speicherstaende-container-id");
+  localStorage.setItem("SpeicherstandAnzeige", currentAnzeige.innerHTML);
 }
 
 function slotButtonClicked(event) {
+  //Wird ausgeführt wenn einer der Speicherstandbuttons geklickt wird und führt die entsprechenden Funktionen aus
   if (!changingSlotName) {
     let element = event.target;
     while (element && !element.classList.contains("slot")) {
@@ -228,47 +216,97 @@ function slotButtonClicked(event) {
     if (element) {
       console.log("slot button: " + element.id + " clicked");
 
-        if (willSlotUmbenennen) {     //Für das Umbenennen des Speicherstandes
-      changingSlotName = true;
-      speicherstandUmbenennen(element);
-
-    }  else if (!willSpeicherstandLaden && !willSlotUmbenennen) {     //Für das Speichern des Speicherstandes
+      if (willSlotUmbenennen) {
+        //Für das Umbenennen des Speicherstandes
         changingSlotName = true;
-        speicherstandUmbenennen(element);
+        slotUmbenennen(element);
+      } else if (!willSpeicherstandLaden && !willSlotUmbenennen) {
+        //Für das Speichern des Speicherstandes
+        changingSlotName = true;
+        slotUmbenennen(element);
         speicherstandSpeichern(element);
-
-  } if (willSpeicherstandLaden) {       //Für das Laden des Speicherstandes
-    console.log("Speicherstand wird geladen");
-    speicherStandLaden();
-    willSpeicherstandLaden = false;
-    speicherStandLadenAnimation();
-      //sdsdd
-  }
-}}
-}
-  
-function speicherStandLaden(){
-
-  
-}
-
-function speicherstandSpeichern(element) {      //Ändert den SpeicherstandArray im localstorage, In porgress
-    let speicherstandArray = JSON.parse(localStorage.getItem("SpeicherstandArray"));
-    let speicherstand = {
-        id: element.id,
-        name: element.children[0].innerHTML,
-        date: element.children[1].innerHTML,
-        speicherTextStelle: textStelle,
-        speicherBildStelle: bildStelle,
-        speicherOptionenStelle: optionenStelle,
-        speicherTextArray: textArray,
+      }
+      if (willSpeicherstandLaden) {
+        //Für das Laden des Speicherstandes
+        console.log("Speicherstand wird geladen");
+        speicherStandLaden(element);
+        willSpeicherstandLaden = false;
+        speicherStandLadenAnimation();
+        //sdsdd
+      }
     }
-    console.log(speicherstand +"wurde gespeichert");
-    speicherstandArray.push(speicherstand);
-    localStorage.setItem("SpeicherstandArray", JSON.stringify(speicherstandArray));
+  }
 }
 
-function speicherstandUmbenennen(element) {     //Nennt nur den Speicherstab um und ändert den SpeicherstandArray im localstorage nicht
+function willSpeicherstandLadenFunc() {
+  willSpeicherstandLaden = !willSpeicherstandLaden;
+  speicherStandLadenAnimation();
+}
+
+function speicherStandLaden(element) {
+  //Lädt den Speicherstand aus dem localstorage und setzt die Werte für den Spielzug macht isTyping false und benutzt ShowCurrentBild
+  let speicherstandArray = JSON.parse(
+    localStorage.getItem("SpeicherstandArray")
+  );
+  let filteredSpeicherstand = speicherstandArray.filter((speicherstand) => {
+    if (speicherstand.id == element.id) {
+      return speicherstand;
+    }
+  });
+  isTyping = false;
+  textStelle = filteredSpeicherstand[0].speicherTextStelle - 1;
+  bildStelle = filteredSpeicherstand[0].speicherBildStelle;
+  optionenStelle = filteredSpeicherstand[0].speicherOptionenStelle;
+  closeMenu();
+  Spielzug();
+  showCurrentBild();
+}
+
+function speicherStandLadenAnimation() {
+  const buttons = document.querySelectorAll(".slot");
+  if (willSpeicherstandLaden) {
+    buttons.forEach((button) => {
+      console.log("Adding bounce to:", button);
+      button.classList.add("bounce");
+    });
+  } else if (!willSpeicherstandLaden || button.classList.contains("bounce")) {
+    buttons.forEach((button) => {
+      console.log("Removing bounce from:", button);
+      button.classList.remove("bounce");
+    });
+  }
+}
+
+function speicherstandSpeichern(element) {
+  //Ändert den SpeicherstandArray im localstorage, In porgress
+  let speicherstandArray = JSON.parse(
+    localStorage.getItem("SpeicherstandArray")
+  );
+  let speicherstand = {
+    id: element.id,
+    name: element.children[0].innerHTML,
+    date: element.children[1].innerHTML,
+    speicherTextStelle: textStelle,
+    speicherBildStelle: bildStelle,
+    speicherOptionenStelle: optionenStelle,
+    speicherTextArray: textArray,
+  };
+  console.log(speicherstand + "wurde gespeichert");
+  speicherstandArray.push(speicherstand);
+  localStorage.setItem(
+    "SpeicherstandArray",
+    JSON.stringify(speicherstandArray)
+  );
+}
+
+function willSlotUmbenennenFunc() {
+  //kehr WILLslotumbenennen um und updated die Visuals
+  willSlotUmbenennen = !willSlotUmbenennen;
+  updateSlotUmbennenVisual();
+}
+
+function slotUmbenennen(element) {
+  //Nennt nur den Speicherstab um und ändert den SpeicherstandArray im localstorage nicht
   element.children[0].innerHTML = `<input id="slot-input-id" class="slot-input" type="text">`;
 
   if (changingSlotName) {
@@ -281,39 +319,41 @@ function speicherstandUmbenennen(element) {     //Nennt nur den Speicherstab um 
         element.children[1].innerHTML = getDate();
         element.children[0].innerHTML = input.value;
         changingSlotName = false;
+        willSlotUmbenennen = false;
         setSpeicherstandAnzeige();
+        updateSlotUmbennenVisual();
       }
     });
   }
 }
 
-function getDate() {
-    var date = new Date();
-    return "Datum: " + date.getDate()+"-"+(date.getMonth() + 1)+"-"+date.getFullYear();
+function updateSlotUmbennenVisual() {
+  // Updated die Visuals während des umbenennen des Speicherstandes, abhänigig von willSlotUmbenennen
+  if (willSlotUmbenennen) {
+    document.querySelectorAll(".slot").forEach((slot) => {
+      slot.style.border = "3px solid #5a0b0b";
+      console.log("slot umbennen visual changed ");
+    });
+  } if (!willSlotUmbenennen) {
+    document.querySelectorAll(".slot").forEach((slot) => {
+      slot.style.border = "3px solid #1abc9c";
+      console.log("slot umbennen visual changed ");
+    });
+  }
 }
 
-onkeydown = function (event) {//öffnet und schließt das Menü mit Escape
-  if (event.code === "Escape") { //Für das Menü, Escape
-    if (menuOpen) {
-      console.log("Menu closed");
-      closeMenu();
-    } else if (!menuOpen) {
-      console.log("Menu opened");
-      openMenu();
-    }
-  }
 
-  if (event.code === "Space") {             //Für den SPielzug, Leertase
-    if (isTyping) {
-        console.log("Text wird vollständig angezeigt");
-      document.getElementById("text").innerHTML = textArray[textStelle - 1];
-      isTyping = false;
-    } else {
-        console.log("Spielzug wird gestartet");
-      Spielzug();
-    }
-  }
-};
+function getDate() {
+  var date = new Date();
+  return (
+    "Datum: " +
+    date.getDate() +
+    "-" +
+    (date.getMonth() + 1) +
+    "-" +
+    date.getFullYear()
+  );
+}
 
 function changeDarkmode() {
   if (localStorage.getItem("dark-mode") === "true") {
@@ -340,7 +380,7 @@ function changeFontSize() {
 }
 changeFontSize(); //Fontsize Einstellen beim Start
 
-window.addEventListener("storage", (event) => {
+window.addEventListener("storage", (event) => {   //wird direkt ausgeführt wenn sich der Localstorage ändert für Fontsize und Darkmode
   //wird direkt ausgeführt wenn sich der Localstorage ändert für Fontsize und Darkmode
   if (event.key == "fontSize") {
     //Fontsize Einstellen mit Localstorage
@@ -350,4 +390,31 @@ window.addEventListener("storage", (event) => {
     //Für Darkmode Einstellungen
     changeDarkmode();
   }
-})
+});
+
+
+onkeydown = function (event) {
+  //öffnet und schließt das Menü mit Escape
+  if (event.code === "Escape") {
+    //Für das Menü, Escape
+    if (menuOpen) {
+      console.log("Menu closed");
+      closeMenu();
+    } else if (!menuOpen) {
+      console.log("Menu opened");
+      openMenu();
+    }
+  }
+
+  if (event.code === "Space") {
+    //Für den SPielzug, Leertase
+    if (isTyping) {
+      console.log("Text wird vollständig angezeigt");
+      document.getElementById("text").innerHTML = textArray[textStelle - 1];
+      isTyping = false;
+    } else {
+      console.log("Spielzug wird gestartet");
+      Spielzug();
+    }
+  }
+};
